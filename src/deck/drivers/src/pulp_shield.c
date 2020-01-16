f8590af655afec4d0e84d29eb4570ce64e23ab57
Jerome <jerome@idsia.ch>
2020-01-16 14:44:34 +0100

    Tabs to spaces

--- src/deck/drivers/src/pulp_shield.c
+++ src/deck/drivers/src/pulp_shield.c
@@ -30,182 +30,182 @@ static float z_;
 static float phi_;
 
 static uint32_t step, lastUpdate, sleep;
-static bool isInit					= false;
-static bool isEnabled 				= false;
+static bool isInit          = false;
+static bool isEnabled         = false;
 
 
 // setpoint_t PULPShieldGetSetpoint() {
-// 	return setpoint_;
+//   return setpoint_;
 // }
 
 
 
 static bool PULPShieldTest() {
 
-	DEBUG_PRINT("First PULP-Shield test passed!\n");
-	// GPIO/SPI test should go here
-	return true;
+  DEBUG_PRINT("First PULP-Shield test passed!\n");
+  // GPIO/SPI test should go here
+  return true;
 }
 
 
 void PULPShieldOn() {
 
-	x_			= 0.0f;
-	y_			= 0.0f;
-	z_			= 0.0f;
-	phi_		= 0.0f;
-	lastUpdate 	= xTaskGetTickCount();
+  x_      = 0.0f;
+  y_      = 0.0f;
+  z_      = 0.0f;
+  phi_    = 0.0f;
+  lastUpdate   = xTaskGetTickCount();
 
-	// Set GPIO on (power on the shield)
-	GPIOOn();
-	// Enable SPI
-	spiBeginSlave();
+  // Set GPIO on (power on the shield)
+  GPIOOn();
+  // Enable SPI
+  spiBeginSlave();
 
-	// Reset task's step counter
-	step = 1;
+  // Reset task's step counter
+  step = 1;
 
   sleep = xTaskGetTickCount();
 
-	// Enable must be set last
-	isEnabled = true;
+  // Enable must be set last
+  isEnabled = true;
 }
 
 
 void PULPShieldOff() {
 
-	// Enable must be set first
-	isEnabled = false;
+  // Enable must be set first
+  isEnabled = false;
 
-	// Set GPIO low
-	GPIOOff();
+  // Set GPIO low
+  GPIOOff();
 
-	// // Reset internal state
-	// resetSetpoint();
+  // // Reset internal state
+  // resetSetpoint();
 
-	// Reset task's step counter
-	step = 1;
+  // Reset task's step counter
+  step = 1;
 }
 
 
 void GPIOInit() {
 
-	//Enable clock for GPIOC
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-	//Initialize struct
-	GPIO_InitTypeDef GPIO_InitStruct;
-	//Pin 12
-	GPIO_InitStruct.GPIO_Pin	= GPIO_Pin_12;
-	//Mode output
-	GPIO_InitStruct.GPIO_Mode	= GPIO_Mode_OUT;
-	//Output type push-pull
-	GPIO_InitStruct.GPIO_OType	= GPIO_OType_PP;
-	//Without pull resistors
-	GPIO_InitStruct.GPIO_PuPd	= GPIO_PuPd_NOPULL;
-	//50MHz pin speed
-	GPIO_InitStruct.GPIO_Speed	= GPIO_Speed_50MHz;
-	//Initialize pins on GPIOG port
-	GPIO_Init(GPIOC, &GPIO_InitStruct);
+  //Enable clock for GPIOC
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+  //Initialize struct
+  GPIO_InitTypeDef GPIO_InitStruct;
+  //Pin 12
+  GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_12;
+  //Mode output
+  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_OUT;
+  //Output type push-pull
+  GPIO_InitStruct.GPIO_OType  = GPIO_OType_PP;
+  //Without pull resistors
+  GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  //50MHz pin speed
+  GPIO_InitStruct.GPIO_Speed  = GPIO_Speed_50MHz;
+  //Initialize pins on GPIOG port
+  GPIO_Init(GPIOC, &GPIO_InitStruct);
 }
 
 
 void GPIOOn() {
 
-	//Set PC12 pin HIGH
-	GPIO_SetBits(GPIOC, GPIO_Pin_12);
+  //Set PC12 pin HIGH
+  GPIO_SetBits(GPIOC, GPIO_Pin_12);
 }
 
 
 void GPIOOff(){
 
-	//Set PC12 pin LOW
-	GPIO_ResetBits(GPIOC, GPIO_Pin_12);
+  //Set PC12 pin LOW
+  GPIO_ResetBits(GPIOC, GPIO_Pin_12);
 }
 
 
 void getPULPOutputs() {
 
-	uint8_t data_rx[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-	uint8_t data_tx[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  uint8_t data_rx[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  uint8_t data_tx[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-	spiBeginTransactionSlave(SPI_BAUDRATE_2MHZ);
-    spiExchangeSlave(8, data_tx, data_rx);
-    spiEndTransactionSlave();
+  spiBeginTransactionSlave(SPI_BAUDRATE_2MHZ);
+  spiExchangeSlave(8, data_tx, data_rx);
+  spiEndTransactionSlave();
 
-  x_ 		= fixed2float((int16_t*)&data_rx[0]);
-	y_		= fixed2float((int16_t*)&data_rx[2]);
-	z_		= fixed2float((int16_t*)&data_rx[4]);
-	phi_	= fixed2float((int16_t*)&data_rx[6]);
+  x_     = fixed2float((int16_t*)&data_rx[0]);
+  y_    = fixed2float((int16_t*)&data_rx[2]);
+  z_    = fixed2float((int16_t*)&data_rx[4]);
+  phi_  = fixed2float((int16_t*)&data_rx[6]);
 
-	DEBUG_PRINT("FRONTNET: %f %f %f %f\n", (double)x_, (double)y_, (double)z_, (double)phi_);
+  DEBUG_PRINT("FRONTNET: %f %f %f %f\n", (double)x_, (double)y_, (double)z_, (double)phi_);
 }
 
 float fixed2float(int16_t *x) {
-	if((~x[0]) < x[0])
-		return -FIXED_BITVALUE * ((~x[0])+1);
-	else
-		return FIXED_BITVALUE * x[0];
+  if((~x[0]) < x[0])
+    return -FIXED_BITVALUE * ((~x[0])+1);
+  else
+    return FIXED_BITVALUE * x[0];
 }
 
 
 uint32_t getLastPULPUpdate() {
-	return lastUpdate;
+  return lastUpdate;
 }
 
 
 static void PULPShieldTask(void *param) {
 
-	DEBUG_PRINT("PULPShieldTask Begin\n");
+  DEBUG_PRINT("PULPShieldTask Begin\n");
 
-	systemWaitStart();
+  systemWaitStart();
 
-	while(1) {
+  while(1) {
 
-	    uint32_t currentTime = xTaskGetTickCount();
-		/**** INITIAL TIMER ****/
-	    if(currentTime-sleep<SLEEP_THRESHOLD) {
-	    	lastUpdate = xTaskGetTickCount();
-	    	vTaskDelay(200);
-	    }
-	    else {
-			if(isEnabled) {
-				if((currentTime-lastUpdate) > PULP_NAVIGATE_RATE) {
-					getPULPOutputs();
-					lastUpdate = xTaskGetTickCount();
-					step++;
-				}
-			} else {
-				vTaskDelay(200);
-			}
-	    }
-	} // close while
+      uint32_t currentTime = xTaskGetTickCount();
+    /**** INITIAL TIMER ****/
+      if(currentTime-sleep<SLEEP_THRESHOLD) {
+        lastUpdate = xTaskGetTickCount();
+        vTaskDelay(200);
+      }
+      else {
+      if(isEnabled) {
+        if((currentTime-lastUpdate) > PULP_NAVIGATE_RATE) {
+          getPULPOutputs();
+          lastUpdate = xTaskGetTickCount();
+          step++;
+        }
+      } else {
+        vTaskDelay(200);
+      }
+      }
+  } // close while
 
-	DEBUG_PRINT("PULPShieldTask Done\n");
+  DEBUG_PRINT("PULPShieldTask Done\n");
 }
 
 static void PULPShieldInit() {
 
-	if(isInit) return;
+  if(isInit) return;
 
-	xTaskCreate(PULPShieldTask,				// pointer to the task entry function
-				PULP_SHIELD_TASK_NAME,		// name for the task
-				PULP_SHIELD_TASK_STACKSIZE,	// number of words for task's stack
-				NULL,						// the task's parameter
-				PULP_SHIELD_TASK_PRI,		// task priority
-				NULL);						// pass a handle
+  xTaskCreate(PULPShieldTask,        // pointer to the task entry function
+        PULP_SHIELD_TASK_NAME,    // name for the task
+        PULP_SHIELD_TASK_STACKSIZE,  // number of words for task's stack
+        NULL,            // the task's parameter
+        PULP_SHIELD_TASK_PRI,    // task priority
+        NULL);            // pass a handle
 
-	GPIOInit();
-	GPIOOff();
-	PULPShieldOn();
+  GPIOInit();
+  // GPIOOff();
+  PULPShieldOn();
 
   isInit = true;
 
-	DEBUG_PRINT("PULP-Shield Initialized\n");
+  DEBUG_PRINT("PULP-Shield Initialized\n");
 }
 
 static const DeckDriver PULPShieldDriver = {
-		.name = "PULPShield",
-		.init = PULPShieldInit,
-		.test = PULPShieldTest,
+    .name = "PULPShield",
+    .init = PULPShieldInit,
+    .test = PULPShieldTest,
 };
 
 
